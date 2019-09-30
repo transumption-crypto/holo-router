9ffbb6e4fd2354b5a94fd4101b82115f20a1d551
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-30 20:14:30 +0000

    Initialize

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import ./pkgs.nix {} }:
+
+with pkgs;
+
+let
+  inherit (rust.packages.nightly) rustPlatform;
+in
+
+{
+  holo-router-agent = buildRustPackage rustPlatform {
+    name = "holo-router-agent";
+    src = gitignoreSource ./.;
+    cargoDir = ".";
+  };
+}
