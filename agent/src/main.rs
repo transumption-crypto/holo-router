f6c5be74307b29689d1d2bb939c9c5ca44c6ca91
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-12-30 00:06:25 +0000

    agent: use hpos-config.json instead of keystore to sign requests

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,59 +1,54 @@
-use holochain_common::DEFAULT_PASSPHRASE;
-use holochain_conductor_api::key_loaders::mock_passphrase_manager;
-use holochain_conductor_api::keystore::{Keystore, PRIMARY_KEYBUNDLE_ID};
-
-use ed25519_dalek::{Keypair, PublicKey, Signature};
-use failure::Error;
+use ed25519_dalek::*;
+use failure::*;
+use hpos_config_core::{Config, public_key};
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::Client;
 use serde::*;
 use zerotier::Identity;
 
 use std::convert::{TryFrom, TryInto};
+use std::env;
+use std::fs;
 use std::time::{SystemTime, UNIX_EPOCH};
-use std::{fs, path::PathBuf};
 
-fn encode_holochain_agent_id<S>(public_key: &PublicKey, serializer: S) -> Result<S::Ok, S::Error>
+fn serialize_holochain_agent_id<S>(public_key: &PublicKey, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    serializer.serialize_str(&base36::encode(&public_key.to_bytes()[..]))
+    serializer.serialize_str(&public_key::to_base36_id(&public_key))
 }
 
-fn encode_instant<S>(time: &SystemTime, serializer: S) -> Result<S::Ok, S::Error>
+fn serialize_instant<S>(time: &SystemTime, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
     serializer.serialize_u128(time.duration_since(UNIX_EPOCH).unwrap().as_millis())
 }
 
-fn header_signature(signature: &Signature) -> HeaderValue {
+fn signature_header(signature: &Signature) -> HeaderValue {
     base64::encode(&signature.to_bytes()[..]).parse().unwrap()
 }
 
 #[derive(Debug, Serialize)]
 struct Payload {
-    #[serde(serialize_with = "encode_instant")]
+    #[serde(serialize_with = "serialize_instant")]
     instant: SystemTime,
-    #[serde(serialize_with = "encode_holochain_agent_id")]
+    #[serde(serialize_with = "serialize_holochain_agent_id")]
     holochain_agent_id: PublicKey,
     zerotier_address: zerotier::Address,
 }
 
-fn holochain_keypair(path: PathBuf) -> Result<Keypair, Error> {
-    let passphrase_manager = mock_passphrase_manager(DEFAULT_PASSPHRASE.into());
-    let mut keystore = Keystore::new_from_file(path, passphrase_manager, None)?;
+fn main() -> Fallible<()> {
+    let config_path = env::var("HPOS_CONFIG_PATH")?;
+    let config_json = fs::read(config_path)?;
+    let Config::V1 { seed, .. } = serde_json::from_slice(&config_json)?;
 
-    let keybundle = keystore.get_keybundle(PRIMARY_KEYBUNDLE_ID)?;
-
-    let mut secret_key_secbuf = keybundle.sign_keys.private;
-    let secret_key_bytes = &**secret_key_secbuf.read_lock();
-
-    Ok(Keypair::from_bytes(&secret_key_bytes[..])?)
-}
-
-fn main() -> Result<(), Error> {
-    let holochain_keypair = holochain_keypair(PathBuf::from("holo-keystore"))?;
+    let holochain_secret_key = SecretKey::from_bytes(&seed)?;
+    let holochain_public_key = PublicKey::from(&holochain_secret_key);
+    let holochain_keypair = Keypair {
+        public: holochain_public_key,
+        secret: holochain_secret_key
+    };
 
     let zerotier_identity =
         Identity::try_from(&fs::read_to_string("/var/lib/zerotier-one/identity.secret")?[..])?;
@@ -62,7 +57,7 @@ fn main() -> Result<(), Error> {
 
     let payload = Payload {
         instant: SystemTime::now(),
-        holochain_agent_id: holochain_keypair.public,
+        holochain_agent_id: holochain_public_key,
         zerotier_address: zerotier_address,
     };
 
@@ -75,12 +70,12 @@ fn main() -> Result<(), Error> {
 
     headers.insert(
         HeaderName::from_static("x-holochain-signature"),
-        header_signature(&holochain_signature),
+        signature_header(&holochain_signature),
     );
 
     headers.insert(
         HeaderName::from_static("x-zerotier-signature"),
-        header_signature(&zerotier_signature),
+        signature_header(&zerotier_signature),
     );
 
     Client::new()
--- default.nix
+++ default.nix
@@ -10,12 +10,10 @@ in
   holo-router-agent = buildRustPackage rustPlatform {
     name = "holo-router-agent";
     src = gitignoreSource ./agent;
-    cargoSha256 = "1gy0wad809p5n62kn6knq0xgrfdawcm9cj4z1kv0d7k8sj2cfnv1";
+    cargoDir = ".";
 
-    RUST_SODIUM_LIB_DIR = "${libsodium}/lib";
-    RUST_SODIUM_SHARED = "1";
-
-    nativeBuildInputs = [ perl ];
+    nativeBuildInputs = [ pkgconfig ];
+    buildInputs = [ openssl ];
 
     meta.platforms = lib.platforms.linux;
   };
